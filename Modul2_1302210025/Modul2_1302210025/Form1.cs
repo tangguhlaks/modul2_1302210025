48dacdc60f9ca47a14f6098a0c8db787d4e58af5
laks <laks@gmail.com>
2023-03-08 15:46:17 +0700

    first

---
+++ Modul2_1302210025/Modul2_1302210025/Form1.cs
@@ -0,0 +1,10 @@
+namespace Modul2_1302210025
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Modul 2 KPL
+Nama	: Tangguh Laksana
+NIM		: 1302210025
+Kelas	: SE-45-01
